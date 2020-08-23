9f224d4a3f741f3419adc7dd208a71d44750ee16
Goglbum <goglbum@gmail.com>
2020-08-23 20:54:00 +0300

    Mili

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        int price = 119;
+        int bonus = 20;
+        int mili = price / bonus;
+        System.out.println(mili);
+    }
+}
